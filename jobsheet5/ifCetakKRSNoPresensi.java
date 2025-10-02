f05942bd4c4b6f5699e772b65bc4ab5a0f47ffb7
RahmansyahAziz28 <rahmansyahaziz67@gmail.com>
2025-10-02 08:16:03 +0700

    tugas 3

--- jobsheet5/ifCetakKRSNoPresensi.java
+++ jobsheet5/ifCetakKRSNoPresensi.java
@@ -6,13 +6,14 @@ public class ifCetakKRSNoPresensi {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        System.out.println("---Cetak KRS---");
-       System.out.println("Apakah ukt sudah lunas? (true/false)");
+       System.out.print("Apakah ukt sudah lunas? (true/false): ");
        boolean lunas = sc.nextBoolean();
-       if (lunas){
-        System.out.println("Pembayaran UKT sudah terverifikasi");
-        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-       } else {
-        System.out.println("“Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-       }
+       System.out.println(lunas ? "Pembayaran UKT sudah terverifikasi" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+    //    if (lunas){
+    //     System.out.println("Pembayaran UKT sudah terverifikasi");
+    //     System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+    //    } else {
+    //     System.out.println("“Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+    //    }
     }
 }
